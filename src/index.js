2c8f182e249995a666a1dd85e290e70ef58d8578
ironylait <ironylait@mail.ru>
2019-03-29 18:23:20 +0300

    refactor: add commits

--- src/index.js
+++ src/index.js
@@ -7,28 +7,28 @@ module.exports = function makeExchange(currency) {
     var Q = 25;
     var D = 10;
     var N = 5;
-    var P;
+    var P = 1;
     var coins = {};
 
-    if (currency / H >= 1) {
+    if (currency / H >= 1) {                //or  (currency >= H)
     coins.H = Math.floor(currency / H);
     currency = currency % H;
-}
+    }
     if (currency / Q >= 1) {
     coins.Q = Math.floor(currency / Q);
     currency = currency % Q;
-}
+    }
     if (currency / D >= 1) {
     coins.D = Math.floor(currency / D);
     currency = currency % D;
-}
+    }
     if (currency / N >= 1) {
     coins.N = Math.floor(currency / N);
     currency = currency % N;
-}
-    if (currency > 0) {
+    }
+    if (currency >= P) {
     coins.P = currency;  
-}
+    }
 
-return coins;
+    return coins;
 }
